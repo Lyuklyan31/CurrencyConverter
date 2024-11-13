3434c7fb75bbba6d50c23281d8ac35bc0bf0a1cd
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-14 00:33:49 +0200

    Update MainViewController.

--- CurrencyConverter/CurrencyConverter/MainViewController.swift
+++ CurrencyConverter/CurrencyConverter/MainViewController.swift
@@ -2,11 +2,38 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    let backgroundView = BackgroundView()
+    let currencyExchangeView = CurrencyExchangeView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        setupUI()
     }
-
     
+    private func setupUI() {
+        setupBackgroundView()
+        setupCurrencyExchangeView()
+    }
+    
+    private func setupBackgroundView() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(backgroundView)
+        backgroundView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview()
+        }
+    }
+    
+    private func setupCurrencyExchangeView() {
+        view.addSubview(currencyExchangeView)
+        currencyExchangeView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(171)
+            $0.horizontalEdges.equalToSuperview()
+        }
+        
+    }
 }
 
+#Preview {
+    MainViewController()
+}
