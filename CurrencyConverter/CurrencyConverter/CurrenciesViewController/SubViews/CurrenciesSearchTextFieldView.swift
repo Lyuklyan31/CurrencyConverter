296992e571c43b02c15fa236f4ff4d86125adb74
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-15 02:56:36 +0200

    Updated SearchTextField.

--- CurrencyConverter/CurrencyConverter/CurrenciesViewController/SubViews/CurrenciesSearchTextFieldView.swift
+++ CurrencyConverter/CurrencyConverter/CurrenciesViewController/SubViews/CurrenciesSearchTextFieldView.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Combine
 import SnapKit
 
 class CurrenciesSearchTextFieldView: UIView {
@@ -14,21 +15,41 @@ class CurrenciesSearchTextFieldView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+//    func  setupBinding() {
+//        searchTextField.textPublisher
+//            .sink { [weak self] textFieldText in
+//                guard let self = self else { return }
+//            }
+//    }
+    
     func setupSearchTextField() {
-        searchTextField.placeholder = "Search Currency"
         let placeholderAttributes: [NSAttributedString.Key: Any] = [
             .foregroundColor: UIColor.placeholderGray,
             .font: UIFont.systemFont(ofSize: 17)
         ]
+        
         searchTextField.textColor = .black
+        searchTextField.attributedPlaceholder = NSAttributedString(string: "Search Currency", attributes: placeholderAttributes)
         
         microphoneButton.setImage(UIImage(systemName: "mic.fill"), for: .normal)
         microphoneButton.frame.size = CGSize(width: 11, height: 16.37)
+        microphoneButton.tintColor = .microphoneGray
+        
+        searchTextField.rightView = microphoneButton
+        searchTextField.rightViewMode = .always
         
         addSubview(searchTextField)
         searchTextField.snp.makeConstraints {
             $0.height.equalTo(36)
             $0.edges.equalToSuperview()
         }
+    }
+}
+
+extension UITextField {
+    var textPublisher: AnyPublisher<String, Never> {
+        NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification, object: self)
+            .compactMap { ($0.object as? UITextField)?.text }
+            .eraseToAnyPublisher()
     }
 }
