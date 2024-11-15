e6144d585afed898cf88e51edd2a3941ee2b2eb2
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-15 04:56:14 +0200

    Created custom cell.

--- CurrencyConverter/CurrencyConverter/CurrenciesViewController/SubViews/CurrencyCell.swift
+++ CurrencyConverter/CurrencyConverter/CurrenciesViewController/SubViews/CurrencyCell.swift
@@ -1,23 +1,34 @@
-//
-//  CurrencyCell.swift
-//  CurrencyConverter
-//
-//  Created by Oleh  on 15.11.2024.
-//
-
+import SnapKit
 import UIKit
 
 class CurrencyCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    private let cellLabel = UILabel()
+    private let backgroundCell = UIView()
+    private let cheсkmark = UIImageView()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupSubViews()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+   private func setupSubViews() {
+        cellLabel.textColor = .black
+        cellLabel.font = UIFont(name: "Lato-Regular", size: 17)
+        cellLabel.numberOfLines = 0
+        
+        addSubview(cellLabel)
+        cellLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.lessThanOrEqualToSuperview()
+            $0.verticalEdges.equalToSuperview().inset(13)
+        }
+    }
+    
+    func configure(with text: String) {
+        cellLabel.text = text
+    }
 }
