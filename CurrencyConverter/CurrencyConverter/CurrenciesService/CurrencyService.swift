72fedc94fa408a1c53ff18cbd6d5ddabb99031e9
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-15 02:56:12 +0200

    Created CurrencyService.

--- CurrencyConverter/CurrencyConverter/CurrenciesService/CurrencyService.swift
+++ CurrencyConverter/CurrencyConverter/CurrenciesService/CurrencyService.swift
@@ -1,8 +1,19 @@
-//
-//  CurrencyService.swift
-//  CurrencyConverter
-//
-//  Created by Oleh  on 15.11.2024.
-//
-
 import Foundation
+
+enum CurrencyServiceError: Error {
+    case fileNotFound(resource: String, fileType: String)
+    case decodingError
+}
+
+class CurrencyService {
+    func fetchCurrencies() throws -> [CurrencyModel] {
+        guard let filepath = Bundle.main.path(forResource: "Currencies", ofType: "json") else {
+            throw CurrencyServiceError.fileNotFound(resource: "Currencies", fileType: "json")
+        }
+        
+        let data = try Data(contentsOf: URL(fileURLWithPath: filepath))
+        let decodedData = try JSONDecoder().decode([String: CurrencyModel].self, from: data)
+        
+        return Array(decodedData.values)
+    }
+}
