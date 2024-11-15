f1e4e6e72fe32f83d62c14f157f6d743fb40bc91
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-15 02:06:54 +0200

    Update CurrenciesViewController.

---
+++ CurrencyConverter/CurrencyConverter/CurrenciesViewController/CurrenciesViewController.swift
@@ -0,0 +1,55 @@
+import SnapKit
+import UIKit
+import Combine
+
+class CurrenciesViewController: UIViewController {
+    private var viewModel: CurrencyViewModel
+
+    private let currencyNavigationBarView = CurrencyNavigationBarView()
+    private let currencySearchTextField = CurrencySearchTextField()
+    
+    private let tableView = UITableView()
+    private var searchTextField = UITextField()
+    
+    private var dataSource: UICollectionViewDiffableDataSource<Int, CurrencyModel>!
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(viewModel: CurrencyViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        setupBinding()
+        currencyNavigationBarView.setupNavBar(for: self)
+        setupCurrencySearchTextField()
+    }
+    
+    private func setupBinding() {
+        
+       
+    }
+    
+    private func setupCurrencySearchTextField() {
+        view.addSubview(currencySearchTextField)
+        currencySearchTextField.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(75)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+        }
+    }
+    
+}
+
+#Preview {
+    CurrenciesViewController(viewModel: CurrencyViewModel())
+}
+
