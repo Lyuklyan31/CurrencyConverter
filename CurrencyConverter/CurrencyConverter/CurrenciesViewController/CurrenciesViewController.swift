36f9cb864bb2e62bc4f19d5a505a32f9aec855ab
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-15 04:56:01 +0200

    Set TableView with DiffableDataSource.

--- CurrencyConverter/CurrencyConverter/CurrenciesViewController/CurrenciesViewController.swift
+++ CurrencyConverter/CurrencyConverter/CurrenciesViewController/CurrenciesViewController.swift
@@ -4,14 +4,13 @@ import Combine
 
 class CurrenciesViewController: UIViewController {
     private var viewModel: CurrencyViewModel
-
+    
     private let currencyNavigationBarView =  CurrenciesNavigationBarView()
     private let currencySearchTextFieldView = CurrenciesSearchTextFieldView()
     
     private let tableView = UITableView()
-    private var searchTextField = UITextField()
     
-    private var dataSource: UICollectionViewDiffableDataSource<Int, CurrencyModel>!
+    private var dataSource: UITableViewDiffableDataSource<Int, CurrencyModel>!
     private var cancellables = Set<AnyCancellable>()
     
     init(viewModel: CurrencyViewModel) {
@@ -26,10 +25,10 @@ class CurrenciesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        configureDefaults()
     }
     
     private func setupUI() {
-        setupBinding()
         currencyNavigationBarView.setupNavBar(for: self)
         setupCurrencySearchTextField()
         setupTableView()
@@ -41,8 +40,13 @@ class CurrenciesViewController: UIViewController {
     }
     
     private func setupBinding() {
-        
-       
+        viewModel.$currencies
+            .receive(on: DispatchQueue.main)
+            .sink { [ weak self ] currencies in
+                guard let self = self else { return }
+                self.applySnapShot(with: currencies)
+            }
+            .store(in: &cancellables)
     }
     
     private func setupCurrencySearchTextField() {
@@ -53,9 +57,47 @@ class CurrenciesViewController: UIViewController {
         }
     }
     
+    private func setupTableView() {
+        
+        tableView.layer.shadowColor = UIColor.black.cgColor
+        tableView.layer.shadowOpacity = 0.15
+        tableView.layer.shadowOffset = CGSize(width: 0, height: 6)
+        tableView.showsVerticalScrollIndicator = false
+        tableView.layer.cornerRadius = 10
+        tableView.delegate = self
+        tableView.register(CurrencyCell.self, forCellReuseIdentifier: "CurrencyCell")
+        
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(currencySearchTextFieldView.snp.bottom).offset(54)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+            $0.bottom.greaterThanOrEqualToSuperview()
+        }
+    }
+    
+    private func setupDataSource() {
+        dataSource = UITableViewDiffableDataSource<Int, CurrencyModel>(tableView: tableView) { tableView, indexPath, currencyData in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "CurrencyCell", for: indexPath) as? CurrencyCell else { return UITableViewCell() }
+            
+            cell.configure(with: "\(currencyData.code) - \(currencyData.name)")
+            
+            return cell
+        }
+    }
+    
+    private func applySnapShot(with currencies: [CurrencyModel]) {
+        var snapShot = NSDiffableDataSourceSnapshot<Int, CurrencyModel>()
+        snapShot.appendSections([0])
+        snapShot.appendItems(currencies, toSection: 0)
+        dataSource.apply(snapShot, animatingDifferences: true)
+    }
+}
+
+extension CurrenciesViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
 }
 
 #Preview {
     CurrenciesViewController(viewModel: CurrencyViewModel())
 }
-
