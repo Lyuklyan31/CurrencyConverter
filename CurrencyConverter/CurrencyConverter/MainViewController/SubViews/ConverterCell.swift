13a6d38846747ad6dd5870c500b9068fb6b7f0b6
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-18 02:22:35 +0200

    Update ConverterCell.

--- CurrencyConverter/CurrencyConverter/MainViewController/SubViews/ConverterCell.swift
+++ CurrencyConverter/CurrencyConverter/MainViewController/SubViews/ConverterCell.swift
@@ -1,23 +1,36 @@
-//
-//  ConverterCell.swift
-//  CurrencyConverter
-//
-//  Created by Oleh  on 17.11.2024.
-//
-
 import UIKit
 
 class ConverterCell: UITableViewCell {
+    let currencyButton = UIButton()
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupSubViews()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
+    
+    private func setupSubViews() {
+        var configuration = UIButton.Configuration.plain()
+        configuration.image = UIImage(systemName: "chevron.right")
+        configuration.imagePadding = 11.04
+        configuration.imagePlacement = .trailing
+        
+        currencyButton.configuration = configuration
+        currencyButton.titleLabel?.font = UIFont(name: "Lato-Regular", size: 14) ?? UIFont.systemFont(ofSize: 14)
+        currencyButton.setTitleColor(.black, for: .normal)
+        
+//        currencyButton.semanticContentAttribute = .forceLeftToRight
 
+        currencyButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(16)
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
+    func configure(with text: String) {
+        currencyButton.setTitle(text, for: .normal)
+    }
 }
