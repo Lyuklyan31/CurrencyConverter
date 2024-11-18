b05c6ad6c23d54589ddfb11f37511f00e980c6f3
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-19 01:31:07 +0200

    Update ConverterCell.

--- CurrencyConverter/CurrencyConverter/MainViewController/SubViews/ConverterCell.swift
+++ CurrencyConverter/CurrencyConverter/MainViewController/SubViews/ConverterCell.swift
@@ -1,8 +1,11 @@
 import UIKit
 
 class ConverterCell: UITableViewCell {
-    let currencyButton = UIButton()
-
+    private let currencyButton = UIButton()
+    private let converterTextField = UITextField()
+    private let backgroundTextFieldView = UIView()
+    private let conteinerView = UIView()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupSubViews()
@@ -13,24 +16,64 @@ class ConverterCell: UITableViewCell {
     }
     
     private func setupSubViews() {
+        contentView.addSubview(conteinerView)
+        conteinerView.snp.makeConstraints {
+            $0.height.equalTo(44)
+            $0.verticalEdges.equalToSuperview().inset(16)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+        }
+        
+        backgroundTextFieldView.backgroundColor = .textFieldBackgroundGrey
+        backgroundTextFieldView.layer.cornerRadius = 6
+        
+        conteinerView.addSubview(backgroundTextFieldView)
+        backgroundTextFieldView.snp.makeConstraints {
+            $0.verticalEdges.right.equalToSuperview()
+            $0.leading.lessThanOrEqualToSuperview().offset(121)
+            $0.right.equalToSuperview()
+        }
+        
+        converterTextField.font = UIFont(name: "Lato-Bold", size: 14)
+        converterTextField.textColor = .black
+        converterTextField.backgroundColor = .textFieldBackgroundGrey
+        converterTextField.placeholder = "0.00"
+        converterTextField.keyboardType = .decimalPad
+        
+        backgroundTextFieldView.addSubview(converterTextField)
+        converterTextField.snp.makeConstraints {
+            $0.height.equalTo(44)
+            $0.edges.equalToSuperview().inset(16)
+        }
+        
         var configuration = UIButton.Configuration.plain()
-        configuration.image = UIImage(systemName: "chevron.right")
-        configuration.imagePadding = 11.04
+        let originalImage = UIImage(systemName: "chevron.right")
+        configuration.image = originalImage?.withTintColor(.black, renderingMode: .alwaysOriginal)
         configuration.imagePlacement = .trailing
+        configuration.imagePadding = 11
         
         currencyButton.configuration = configuration
         currencyButton.titleLabel?.font = UIFont(name: "Lato-Regular", size: 14) ?? UIFont.systemFont(ofSize: 14)
         currencyButton.setTitleColor(.black, for: .normal)
+        currencyButton.addTarget(self, action: #selector(currencyButtonTapped), for: .touchUpInside)
+        currencyButton.titleLabel?.lineBreakMode = .byTruncatingTail
         
-//        currencyButton.semanticContentAttribute = .forceLeftToRight
-
+        conteinerView.addSubview(currencyButton)
         currencyButton.snp.makeConstraints {
-            $0.leading.equalToSuperview().offset(16)
-            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview()
+            $0.right.greaterThanOrEqualToSuperview().offset(-279)
+            $0.verticalEdges.equalToSuperview()
         }
+    }
+    
+    @objc func currencyButtonTapped() {
+        print("Currency button tapped")
     }
     
     func configure(with text: String) {
         currencyButton.setTitle(text, for: .normal)
     }
+}
+
+#Preview {
+    ConverterCell()
 }
