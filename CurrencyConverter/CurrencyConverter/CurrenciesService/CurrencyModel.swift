2e0ae6c6c96236c0d3669ae6b5dc380914fbe0bf
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-19 01:29:27 +0200

    Done Hashable.

--- CurrencyConverter/CurrencyConverter/CurrenciesService/CurrencyModel.swift
+++ CurrencyConverter/CurrencyConverter/CurrenciesService/CurrencyModel.swift
@@ -1,6 +1,19 @@
 import Foundation
 
 struct CurrencyModel: Codable, Hashable {
+    internal init(name: String, code: String) {
+        self.name = name
+        self.code = code
+    }
+    
     let name: String
     let code: String
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(code)
+    }
+
+    static func == (lhs: CurrencyModel, rhs: CurrencyModel) -> Bool {
+        return lhs.code == rhs.code
+    }
 }
