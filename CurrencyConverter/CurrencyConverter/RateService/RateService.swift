97ba512eb0a4b44ad6a3ae8aff6d01a89898acec
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-19 17:58:12 +0200

    Created RateService.

--- CurrencyConverter/CurrencyConverter/RateService/RateService.swift
+++ CurrencyConverter/CurrencyConverter/RateService/RateService.swift
@@ -1,8 +1,18 @@
-//
-//  CurrencyService.swift
-//  CurrencyConverter
-//
-//  Created by Oleh  on 19.11.2024.
-//
-
 import Foundation
+
+class RateService {
+    func fetchRates() async throws -> RateModel {
+        let apiKey = APIKeyProvider.getAPIKey()
+        
+        let urlString = "https://v6.exchangerate-api.com/v6/\(apiKey)/latest/USD"
+        
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let decodedResponse = try JSONDecoder().decode(RateModel.self, from: data)
+        
+        return decodedResponse
+    }
+}
