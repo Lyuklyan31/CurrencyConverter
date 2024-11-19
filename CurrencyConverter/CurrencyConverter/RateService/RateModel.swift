2dfec421505895d0218d8d8fda640752a9d0066a
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-19 17:57:50 +0200

    Created RateModel.

--- CurrencyConverter/CurrencyConverter/RateService/RateModel.swift
+++ CurrencyConverter/CurrencyConverter/RateService/RateModel.swift
@@ -1,8 +1,11 @@
-//
-//  RateModel.swift
-//  CurrencyConverter
-//
-//  Created by Oleh  on 19.11.2024.
-//
-
 import Foundation
+
+struct RateModel: Codable {
+    let baseCode: String
+    let conversionRates: [String: Double]
+
+    enum CodingKeys: String, CodingKey {
+        case baseCode = "base_code"
+        case conversionRates = "conversion_rates"
+    }
+}
