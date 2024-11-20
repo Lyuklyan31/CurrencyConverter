4b36b4573020ad4b2c0f1395f4f19d9e9b4c42b4
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-20 04:58:04 +0200

    Update ConverterCell.

--- CurrencyConverter/CurrencyConverter/MainViewController/SubViews/ConverterCell.swift
+++ CurrencyConverter/CurrencyConverter/MainViewController/SubViews/ConverterCell.swift
@@ -2,40 +2,63 @@ import UIKit
 import Combine
 
 class ConverterCell: UITableViewCell {
-    var viewModel = CurrencyViewModel()
-
+    var viewModel: CurrencyViewModel!
+    
     private let currencyButton = UIButton()
     private let converterTextField = UITextField()
     private let backgroundTextFieldView = UIView()
-    private let conteinerView = UIView()
+    private let containerView = UIView()
     
-    private var textCode = ""
     private var cancellables = Set<AnyCancellable>()
+    private var textCode = ""
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupSubViews()
+        setupSubviews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setupSubViews() {
-        contentView.addSubview(conteinerView)
-        conteinerView.snp.makeConstraints {
+    private func setupBindings() {
+        converterTextField.textPublisher
+            .compactMap { text in
+                return Double(text)
+            }
+            .sink { [weak self] value in
+                guard let self = self else { return }
+                self.viewModel.updateValue(for: self.textCode, newValue: value)
+            }
+            .store(in: &cancellables)
+
+        viewModel.$converterList
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] converterList in
+                guard let self = self else { return }
+                guard let currentCurrency = converterList.first(where: { $0.code == self.textCode }) else { return }
+                
+                if !self.converterTextField.isFirstResponder {
+                    self.converterTextField.placeholder = String(format: "%.2f", currentCurrency.value)
+                }
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func setupSubviews() {
+        contentView.addSubview(containerView)
+        containerView.snp.makeConstraints {
             $0.verticalEdges.equalToSuperview().inset(8)
             $0.horizontalEdges.equalToSuperview().inset(16)
         }
         
         backgroundTextFieldView.backgroundColor = .textFieldBackgroundGrey
         backgroundTextFieldView.layer.cornerRadius = 6
-        
-        conteinerView.addSubview(backgroundTextFieldView)
+        containerView.addSubview(backgroundTextFieldView)
         backgroundTextFieldView.snp.makeConstraints {
             $0.height.equalTo(44)
             $0.leading.lessThanOrEqualToSuperview().offset(121)
-            $0.right.equalToSuperview()
+            $0.trailing.equalToSuperview()
         }
         
         converterTextField.font = UIFont(name: "Lato-Bold", size: 14)
@@ -64,35 +87,25 @@ class ConverterCell: UITableViewCell {
         currencyButton.addTarget(self, action: #selector(currencyButtonTapped), for: .touchUpInside)
         currencyButton.titleLabel?.lineBreakMode = .byTruncatingTail
         
-        conteinerView.addSubview(currencyButton)
+        containerView.addSubview(currencyButton)
         currencyButton.snp.makeConstraints {
             $0.leading.equalToSuperview()
-            $0.right.greaterThanOrEqualToSuperview().offset(-279)
+            $0.trailing.greaterThanOrEqualToSuperview().offset(-279)
             $0.height.equalTo(44)
             $0.top.equalToSuperview()
             $0.bottom.equalToSuperview()
         }
     }
     
-    private func setupBinding() {
-        viewModel.$conversionRates
-            .sink { [weak self] rate in
-                guard let self = self else { return }
-                let rateText = rate[self.textCode] ?? 0.0
-                DispatchQueue.main.async {
-                    self.converterTextField.placeholder = String(format: "%.2f", rateText)
-                }
-            }
-            .store(in: &cancellables)
-    }
-
     @objc func currencyButtonTapped() {
         print("Currency button tapped")
     }
-
-    func configure(with text: String) {
-        textCode = text
-        currencyButton.setTitle(text, for: .normal)
-        setupBinding()
+    
+    func configure(with code: String, value: Double, viewModel: CurrencyViewModel) {
+        self.textCode = code
+        self.viewModel = viewModel
+        currencyButton.setTitle(code, for: .normal)
+        converterTextField.placeholder = String(format: "%.2f", value)
+        setupBindings()
     }
 }
