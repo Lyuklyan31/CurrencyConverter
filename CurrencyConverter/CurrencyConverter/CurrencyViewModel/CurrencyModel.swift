2aec9f1a2052c876e8870878f5dd70713069aa9f
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-20 23:12:18 +0200

    Update CurrencyModel.

--- CurrencyConverter/CurrencyConverter/CurrencyViewModel/CurrencyModel.swift
+++ CurrencyConverter/CurrencyConverter/CurrencyViewModel/CurrencyModel.swift
@@ -1,15 +1,21 @@
 import Foundation
 
 struct CurrencyModel: Codable, Hashable {
+//    var id = UUID()
     let name: String
     let code: String
     var value: Double
+    var isSelected: Bool = false
     
     func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
         hasher.combine(code)
+        hasher.combine(value)
     }
 
     static func == (lhs: CurrencyModel, rhs: CurrencyModel) -> Bool {
-        return lhs.code == rhs.code
+        return lhs.name == rhs.name &&
+        lhs.code == rhs.code &&
+        lhs.value == rhs.value
     }
 }
