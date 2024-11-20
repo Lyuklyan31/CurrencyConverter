8e07b2d8380ea0bb1775e9f7fa8ab649438399dd
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-20 04:56:29 +0200

    Update ViewModel.

--- CurrencyConverter/CurrencyConverter/CurrencyViewModel.swift
+++ CurrencyConverter/CurrencyConverter/CurrencyViewModel.swift
@@ -1,22 +1,33 @@
-import Foundation
 import Combine
+import UIKit
 
 class CurrencyViewModel {
     private var currencyService = CurrencyService()
     private let rateService = RateService()
     
     @Published private(set) var currencies = [CurrencyModel]()
-    @Published private(set) var converterList = [CurrencyModel]() {
-        didSet {
-            saveConverterList()
-        }
+    @Published private(set) var converterList = [ConverterCurrencyModel]() {
+        didSet { saveConverterList() }
     }
     @Published private(set) var alertMessage: String?
     @Published private(set) var conversionRates = [String: Double]()
     
     private var allCurrencies = [CurrencyModel]()
-    
     private let userDefaultsKey = "converterList"
+    
+    struct ConverterCurrencyModel: Codable, Hashable {
+        let name: String
+        let code: String
+        var value: Double
+        
+        func hash(into hasher: inout Hasher) {
+            hasher.combine(code)
+        }
+
+        static func == (lhs: ConverterCurrencyModel, rhs: ConverterCurrencyModel) -> Bool {
+            return lhs.code == rhs.code
+        }
+    }
     
     init() {
         fetchCurrencies()
@@ -45,19 +56,38 @@ class CurrencyViewModel {
     }
     
     func updateConverterList(at indexPath: Int) {
-        converterList.append(currencies[indexPath])
+        let selectedCurrency = currencies[indexPath]
+        guard !converterList.contains(where: { $0.code == selectedCurrency.code }) else { return }
+        let newCurrency = ConverterCurrencyModel(name: selectedCurrency.name, code: selectedCurrency.code, value: 0.0)
+        converterList.append(newCurrency)
+    }
+    
+    func updateValue(for code: String, newValue: Double) {
+        guard let baseRate = conversionRates[code] else { return }
+        
+        converterList = converterList.map { currency in
+            if currency.code == code {
+                return ConverterCurrencyModel(name: currency.name, code: currency.code, value: newValue)
+            } else {
+                guard let currentRate = conversionRates[currency.code] else { return currency }
+                let convertedValue = (newValue / baseRate) * currentRate
+                return ConverterCurrencyModel(name: currency.name, code: currency.code, value: convertedValue)
+            }
+        }
     }
     
     private func saveConverterList() {
-        let data = converterList.map { ["name": $0.name, "code": $0.code] }
+        let data = converterList.map { ["name": $0.name, "code": $0.code, "value": $0.value] }
         UserDefaults.standard.set(data, forKey: userDefaultsKey)
     }
     
     private func loadConverterList() {
-        guard let savedData = UserDefaults.standard.array(forKey: userDefaultsKey) as? [[String: String]] else { return }
+        guard let savedData = UserDefaults.standard.array(forKey: userDefaultsKey) as? [[String: Any]] else { return }
         converterList = savedData.compactMap { dict in
-            if let name = dict["name"], let code = dict["code"] {
-                return CurrencyModel(name: name, code: code)
+            if let name = dict["name"] as? String,
+               let code = dict["code"] as? String,
+               let value = dict["value"] as? Double {
+                return ConverterCurrencyModel(name: name, code: code, value: value)
             }
             return nil
         }
@@ -67,8 +97,6 @@ class CurrencyViewModel {
         if let savedRatesData = UserDefaults.standard.data(forKey: "conversionRates"),
            let savedRates = try? JSONDecoder().decode(RateModel.self, from: savedRatesData) {
             conversionRates = savedRates.conversionRates
-        } else {
-
         }
     }
     
@@ -76,11 +104,10 @@ class CurrencyViewModel {
         Task {
             do {
                 let rateModel = try await rateService.fetchRates()
-                
                 saveExchangeRates(rateModel)
                 conversionRates = rateModel.conversionRates
             } catch {
-               
+                alertMessage = "Error fetching exchange rates."
             }
         }
     }
@@ -89,9 +116,5 @@ class CurrencyViewModel {
         if let encodedRates = try? JSONEncoder().encode(rateModel) {
             UserDefaults.standard.set(encodedRates, forKey: "conversionRates")
         }
-    }
-    
-    private func ccalculationRate(number: Double) {
-        
     }
 }
