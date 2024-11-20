932d314bc2d67ad2748fcfba014d1a6b030c31aa
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-20 04:56:09 +0200

    Created TapDismiss Keyboard.

---
+++ CurrencyConverter/CurrencyConverter/TapToDismissKeyboardExtension.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// MARK: - Setup Dismiss Keyboard Gesture
+extension UIViewController {
+    func setupDismissKeyboard() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
