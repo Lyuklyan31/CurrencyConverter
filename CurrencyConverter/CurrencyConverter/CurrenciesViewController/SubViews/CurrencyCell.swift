c6754c035434b869c1055500c4133ca2c8235617
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-21 02:26:19 +0200

    Added checkMark in Cell.

--- CurrencyConverter/CurrencyConverter/CurrenciesViewController/SubViews/CurrencyCell.swift
+++ CurrencyConverter/CurrencyConverter/CurrenciesViewController/SubViews/CurrencyCell.swift
@@ -3,6 +3,7 @@ import UIKit
 
 class CurrencyCell: UITableViewCell {
     private let cellLabel = UILabel()
+    private var checkmarkUIImageView = UIImageView()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -24,9 +25,30 @@ class CurrencyCell: UITableViewCell {
             $0.trailing.lessThanOrEqualToSuperview()
             $0.verticalEdges.equalToSuperview().inset(13)
         }
+        
+        checkmarkUIImageView.image = UIImage(systemName: "circle")
+        checkmarkUIImageView.tintColor = .systemGreen
+        checkmarkUIImageView.contentMode = .scaleAspectFit
+        addSubview(checkmarkUIImageView)
+        
+        checkmarkUIImageView.snp.makeConstraints {
+            $0.trailing.equalToSuperview().offset(-16)
+            $0.centerY.equalToSuperview()
+            $0.leading.greaterThanOrEqualTo(cellLabel.snp.trailing)
+        }
     }
     
     func configure(with text: String) {
         cellLabel.text = text
+    }
+    
+    func applyCheckedLook() {
+        checkmarkUIImageView.image = UIImage(systemName: "checkmark.circle.fill")
+        checkmarkUIImageView.tintColor = .systemGreen
+    }
+    
+    func applyUncheckedLook() {
+        checkmarkUIImageView.image = UIImage(systemName: "circle")
+        checkmarkUIImageView.tintColor = .systemGreen
     }
 }
